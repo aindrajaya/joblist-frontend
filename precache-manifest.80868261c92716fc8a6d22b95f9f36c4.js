f37be904b0a67bea74ea0d0b8b09e189f7afcbdd
Arista Indrajaya <arista.indrajaya@gmail.com>
2020-05-31 08:26:49 +0700

    Updates

---
+++ precache-manifest.80868261c92716fc8a6d22b95f9f36c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67efc73d83078df18067eeaaebaad5a1",
+    "url": "/joblist-frontend/index.html"
+  },
+  {
+    "revision": "b84371d4d41cf1cf29b2",
+    "url": "/joblist-frontend/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b2798dacd5bc7f89caca",
+    "url": "/joblist-frontend/static/js/2.edf2ea26.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/joblist-frontend/static/js/2.edf2ea26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b84371d4d41cf1cf29b2",
+    "url": "/joblist-frontend/static/js/main.b71d78b3.chunk.js"
+  },
+  {
+    "revision": "e44da7df81fbac4ef457",
+    "url": "/joblist-frontend/static/js/runtime-main.b0673145.js"
+  }
+]);
